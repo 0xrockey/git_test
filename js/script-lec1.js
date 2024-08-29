6d1de4d3fb2029ac94621a86215b32e5b85c54e9
0xrockey <rocky.rocket.dev@gmail.com>
2024-08-29 18:59:40 +0200

    work with number

--- js/script-lec1.js
+++ js/script-lec1.js
@@ -20,3 +20,14 @@ console.log("output :" + pi);
 pi = 14;
 // we will get error
 console.log(pi);
+
+// number are the building blocks of programming
+//  ex  (3+2)-76*(1+1) is also vaild
+// we use console,log to out the number also
+// Assignment
+
+console.log("add :" + (23 + 97));
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a * 9);

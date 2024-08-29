b90a74883b92fc23600429849b57518494b0ad01
0xrockey <rocky.rocket.dev@gmail.com>
2024-08-29 15:16:31 +0200

    use var variable

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // ues variable
-// let
+// let we can re-assgin
 
 let firstname = "ko";
 let surname = "la";
 
 console.log(firstname);
 console.log(surname);
+
+// var we can re-assign
+var x = 10;
+console.log("var x : " + x);
+x = 15;
+console.log("var new x : " + x);
